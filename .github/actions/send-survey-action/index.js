8379dbf0ebd35ced3e07c37b432c7946dca109dc
Rob Dodson <robdodson@google.com>
2019-10-05 00:17:29 -0700

    Attempt to log issues.

--- .github/actions/send-survey-action/index.js
+++ .github/actions/send-survey-action/index.js
@@ -1,15 +1,19 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+  const {repository} = await octokit.graphql(
+    `
+      {
+        repository(owner: "robdodson", name: "cloud-build-tester") {
+          issues(last: 3) {
+            edges {
+              node {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  console.log(repository);
+})();
