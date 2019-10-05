4b70eba5e66b23c216bcb5c27e38b36ea0a5393b
Rob Dodson <robdodson@google.com>
2019-10-05 14:33:25 -0700

    await chaining!

--- .github/actions/send-survey-action/index.js
+++ .github/actions/send-survey-action/index.js
@@ -1,24 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-const {context} = github;
-const CONTENT_LABEL = 'content proposal';
+const { context } = github;
+const CONTENT_LABEL = "content proposal";
 
 (async () => {
   try {
-    const hasContentLabel = await octokit.issues.listLabelsOnIssue({
-          ...context.repo,
-          issue_number: context.issue.number,
-        }).map(label => label.name).includes(CONTENT_LABEL);
+    const hasContentLabel = (await octokit.issues.listLabelsOnIssue({
+      ...context.repo,
+      issue_number: context.issue.number
+    }))
+      .map(label => label.name)
+      .includes(CONTENT_LABEL);
 
     if (hasContentLabel) {
       await octokit.issues.createComment({
         ...context.repo,
         issue_number: context.issue.number,
-        body: 'Do a barrel roll, @robdodson!'
+        body: "Do a barrel roll, @robdodson!"
       });
     }
-  } catch(err) {
+  } catch (err) {
     core.setFailed(err.message);
   }
 })();
